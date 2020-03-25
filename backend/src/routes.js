fd2b8746f209206c0a1ede30eccfa84d26dbc237
SusanaMCosta <suzanasuzy12@hotmail.com>
2020-03-25 19:32:00 -0300

    aula 2

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,26 +1,27 @@
 const express = require('express');
-const crypto = require('crypto');
 
-const connection = require('./database/connection');
+const OngController = require('./controllers/OngController');
+
+const IncidentController = require('./controllers/IncidentController');
+
+const ProfileController = require('./controllers/ProfileController');
+
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/ongs', async (request, response) => {
-    const {name, email, whatsapp, city, uf} = request.body;
+routes.post('/sessions', SessionController.create);
 
-    const id = crypto.randomBytes(4).toString('HEX');
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    });
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
 
-    return response.json({ id });
-});
+routes.get('/profile', ProfileController.index);
+
+
 
 module.exports = routes;
 
